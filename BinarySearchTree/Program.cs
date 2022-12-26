5748a2218c18874d43dab66688e9538a56219fd7
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2022-12-26 18:17:10 +0800

    repair class node

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -1,24 +1,25 @@
 ﻿namespace BinarySearchTree
 {
+    class node
+    {
+        public string info;
+        public node lchild;
+        public node rchild;
+
+        //constructor for the node class
+
+        public node(string i, node l, node r)
+        {
+            info = i;
+            lchild = l;
+            rchild = r;
+        }
+    }
     class Program
     {
         /* A node class consi
          * 
          */
-        class node
-        {
-            public string info;
-            public node lchild;
-            public node rchild;
-
-            //constructor for the node class
-
-            public node(string i, node l, node r)
-            {
-                info = i;
-                lchild = l;
-                rchild = r;
-            }
-        }
+        
     }
 }
