a5956a6b4543d0556cb2d41905fdea30c7ee2768
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2022-12-26 18:41:43 +0800

    create function in public void inorder

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -62,6 +62,20 @@
                     currentnode = currentnode.rchild;
             }
         }
+        public void inorder(node ptr)
+        {
+            if (ROOT == null)
+            {
+                Console.WriteLine("Tree is Empty");
+                return;
+            }
+            if (ptr != null)
+            {
+                inorder(ptr.lchild);
+                Console.WriteLine(ptr.info + "");
+                inorder(ptr.rchild);
+            }
+        }
     }
 
 
