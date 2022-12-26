a24fb90858f062ab3f394860895acba35b6d59ae
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2022-12-26 18:15:21 +0800

    create node in class node

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -1,4 +1,24 @@
 ﻿namespace BinarySearchTree
 {
-    
+    class Program
+    {
+        /* A node class consi
+         * 
+         */
+        class node
+        {
+            public string info;
+            public node lchild;
+            public node rchild;
+
+            //constructor for the node class
+
+            public node(string i, node l, node r)
+            {
+                info = i;
+                lchild = l;
+                rchild = r;
+            }
+        }
+    }
 }
